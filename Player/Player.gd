4b15129e856b111f7d3226b1950044290ee45c15
karapnaran <mesropandreasyan@gmail.com>
2021-09-09 01:21:39 +0400

    first commit

---
+++ Player/Player.gd
@@ -0,0 +1,55 @@
+extends KinematicBody2D
+
+const MAX_SPEED = 100
+
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var state = MOVE
+var velocity = Vector2.ZERO
+
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
+
+func _ready():
+	animationTree.active = true
+	
+func _process(delta):
+	match state:
+		MOVE:
+			move_state()
+		ROLL:
+			pass
+		ATTACK:
+			attack_state()
+	 
+func move_state():
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationTree.set("parameters/Attack/blend_position", input_vector)
+		animationState.travel("Run")
+		velocity = input_vector * MAX_SPEED
+	else:
+		animationState.travel("Idle")
+		velocity = Vector2.ZERO
+		
+	velocity = move_and_slide(velocity)
+	
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+
+func attack_state():
+	animationState.travel("Attack")
+	
+func attack_animation_finished():
+	state = MOVE	
